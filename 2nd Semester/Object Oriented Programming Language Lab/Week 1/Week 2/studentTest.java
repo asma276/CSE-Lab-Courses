7e8f10250f727042a665b9753dd7926512d1be8b
asma276 <688asmaakter@gmail.com>
2025-01-26 04:49:20 +0600

    Delete 2nd Semester directory

--- 2nd Semester/Object Oriented Programming Language Lab/Week 1/Week 2/studentTest.java
+++
@@ -1,7 +0,0 @@
-public class studentTest {
-    public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.setInformation("Limon", "Male", 1710444707);
-        student1.displayInformation();
-    }
-}
--- 2nd Semester/Object Oriented Programming Language Lab/Week 1/Week 2/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-public static void main(String[] args) {
-        int n =4;
-
-        //outer loop
-        for(int i=1; i<=n; i++) {
